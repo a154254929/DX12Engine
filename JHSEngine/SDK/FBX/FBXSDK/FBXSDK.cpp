3702d295a4022c84db3e5bd0cb54ba8562c5d29e
菊黄射 <154254929@qq.com>
2026-08-02 16:10:00 +0800

    第十七章3

--- JHSEngine/SDK/FBX/FBXSDK/FBXSDK.cpp
+++ JHSEngine/SDK/FBX/FBXSDK/FBXSDK.cpp
@@ -4,6 +4,27 @@
 #pragma comment(lib, "libfbxsdk-md.lib")
 #pragma comment(lib, "libfbxsdk-mt.lib")
 
+void InitializeSdkObjects(FbxManager*& inManager, FbxScene*& inScene)
+{
+    inManager = FbxManager::Create();
+    
+    FbxIOSettings* fbxIO = FbxIOSettings::Create(inManager, IOSROOT);
+    inManager->SetIOSettings(fbxIO);
+    
+    FbxString fbxPath = FbxGetApplicationDirectory();
+    inManager->LoadPluginsDirectory(fbxPath);
+    
+    inScene = FbxScene::Create(inManager, "My Scene");
+}
+
+void FFBXAssetImport::LoadMeshData(const std::string& inPath, FFBXRenderData& outRenderData)
+{
+    FbxManager* manager = NULL;
+    FbxScene* scene = NULL;
+    
+    InitializeSdkObjects(manager, scene);
+}
+
 int main()
 {
     FbxManager* manager = FbxManager::Create();
--- JHSEngine/SDK/FBX/FBXSDK/FBXSDK.h
+++ JHSEngine/SDK/FBX/FBXSDK/FBXSDK.h
@@ -1,1 +1,16 @@
 #pragma once
+#include <iostream>
+
+struct FFBXRenderData
+{
+        //index
+        //点位置
+        //法线
+        //切线
+        //纹理坐标
+};
+
+struct FFBXAssetImport
+{
+   void  LoadMeshData(const std::string& inPath, FFBXRenderData& outRenderData);
+};
