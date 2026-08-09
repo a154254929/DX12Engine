bac4ef4daead677cca2adedf9e068e48f47a99dd
菊黄射 <154254929@qq.com>
2026-08-09 17:11:13 +0800

    第十八章15

--- JHSEngine/Engine/Math/EngineMath.cpp
+++ JHSEngine/Engine/Math/EngineMath.cpp
@@ -29,4 +29,40 @@ namespace EngineMath
         float phi = atan2f(inV3d.y, inV3d.x);
         return fvector_3d(radius, theta, phi);
     }
+
+    struct FCubemapAxialRangeR
+    {
+        FCubemapAxialRangeR()
+            : PositiveX(fvector_4d(XM_PIDIV4, XM_PIDIV4 * 3, XM_PIDIV4, XM_PIDIV4* 3))
+            , NegativeX(fvector_4d(XM_PIDIV4, XM_PIDIV4 * 3, XM_PIDIV4 * 5, -XM_PIDIV4 * 7))
+            , PositiveY(fvector_4d(0, XM_PIDIV4, 0, XM_PI * 2))
+            , NegativeY(fvector_4d(XM_PIDIV4 * 3, XM_PI, 0, XM_PI * 2))
+            , PositiveZ(fvector_4d(XM_PIDIV4, XM_PIDIV4 * 3, -XM_PIDIV4, XM_PIDIV4))
+            , NegativeZ(fvector_4d(XM_PIDIV4, XM_PIDIV4 * 3, XM_PIDIV4 * 3, XM_PIDIV4 * 5))
+        {
+            
+        }
+        const fvector_4d PositiveX;
+        const fvector_4d NegativeX;
+        
+        const fvector_4d PositiveY;
+        const fvector_4d NegativeY;
+        
+        const fvector_4d PositiveZ;
+        const fvector_4d NegativeZ;
+    };
+    
+    static FCubemapAxialRangeR cubemapAxialRangeR;
+    
+    ECubeMapFace GetSampleCubemapIndex(fvector_3d inPointPosition)
+    {
+        fvector_3d sphericalCoords = GetPointSphericalCoordinates(inPointPosition);
+        float theta = sphericalCoords.y;
+        float phi = sphericalCoords.z;
+
+        
+        
+        
+        return Invalid;
+    }
 }
--- JHSEngine/Engine/Math/EngineMath.h
+++ JHSEngine/Engine/Math/EngineMath.h
@@ -3,6 +3,17 @@
 
 namespace EngineMath
 {
+    enum ECubeMapFace
+    {
+        PositiveX  = 0, 
+        NegativeX = 1,
+        PositiveY = 2,
+        NegativeY = 3,
+        PositiveZ = 4,
+        NegativeZ = 5,
+        Invalid = 6
+    };
+    
     XMFLOAT4X4 IdentityMatrix4x4();
     
     XMFLOAT3 ToFloat3(const fvector_3d& inV3d);
@@ -10,4 +21,6 @@ namespace EngineMath
     fvector_3d ToVector3d(const XMFLOAT3& inV3d);
     
     fvector_3d GetPointSphericalCoordinates(const fvector_3d& inV3d);
+    
+    ECubeMapFace GetSampleCubemapIndex(fvector_3d inPointPosition);
 }
